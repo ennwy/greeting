a20a2291ecd2699caf19027e788f68075989be22
ennwy <ennwyexe@gmail.com>
2022-02-01 23:11:29 +0200

    Some edits

--- config.py
+++ config.py
@@ -6,12 +6,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
-    "QOrNX~,BJg\`K;U|C`K:'U?~84_~~q$l"
+    "write-your-secret-key"
     
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'users.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_ENV = os.environ.get('FLASK_ENV')
